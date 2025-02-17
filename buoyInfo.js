ea932cc5e98a2ab25755f722bcf81877cabb1ba2
Ivan Dalla Ragione <94239465+Ivanettoss@users.noreply.github.com>
2025-02-17 17:08:51 +0100

    Formal view : sidebar + card + full map

--- buoyInfo.js
+++ buoyInfo.js
@@ -2,12 +2,22 @@
 function showBuoyInfo(buoyNumber, latlng) {
     var buoyInfoDiv = document.getElementById('buoyInfo');
     
-    document.getElementById('buoyId').textContent = buoyNumber;
-    document.getElementById('lat').textContent = latlng.lat.toFixed(6); // Mostra la latitudine con 6 decimali
-    document.getElementById('long').textContent = latlng.lng.toFixed(6); // Mostra la longitudine con 6 decimali
+    if (!buoyInfoDiv) {
+        console.error('Elemento con id "buoyInfo" non trovato.');
+        return;
+    }
 
-    buoyInfoDiv.style.display = 'block'; // Mostra il div
+    if (latlng && typeof latlng.lat === 'number' && typeof latlng.lng === 'number') {
+        document.getElementById('buoyId').textContent = buoyNumber;
+        document.getElementById('lat').textContent = latlng.lat.toFixed(6); // Mostra la latitudine con 6 decimali
+        document.getElementById('long').textContent = latlng.lng.toFixed(6); // Mostra la longitudine con 6 decimali
+
+        buoyInfoDiv.style.display = 'block'; // Mostra il div
+    } else {
+        console.error('Latitudine e longitudine non valide');
+    }
 }
+
 
 // Funzione per nascondere il div
 function hideBuoyInfo() {
